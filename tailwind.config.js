06efecb1657bc3da55dd880a47bf5c8250d6dc0e
Josevaldo <josevaldosicuba@gmai.com>
2023-12-11 21:52:00 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+          primary: "#2563eb",
+          secundary: "#22c55e",
+      },
+    },
+  },
+  plugins: [],
+}
+
